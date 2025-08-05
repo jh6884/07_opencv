0b06e38fa49da5cff0ab9ce3fdf94507c5b55110
shinjaehyeon <pumpkinalpha996@gmail.com>
2025-08-05 17:09:28 +0900

    마우스 클릭 이벤트 작동 확인

--- src/color_classify.py
+++ src/color_classify.py
@@ -1,6 +1,7 @@
 import numpy as np, cv2
 import matplotlib.pyplot as plt
-img = None
+import csv
+
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
@@ -15,25 +16,32 @@ colors = [
     'white',
     'gray'
 ]
-
-while cap.isOpened():
-    ret, frame = cap.read()
-    
-
 def train_event(event, x, y, flags, param):
-    global frame
+    global rgb, hsv, img
     if event == cv2.EVENT_LBUTTONDOWN:
-        color = frame[x, y]
-        cv2.circle(frame, (x, y), 3, (int(color[0]), int(color[1]), int(color[3])), -1)
+        color = rgb[y, x]
+        cv2.circle(img, (x, y), 3, (int(color[2]), int(color[1]), int(color[0])), -1)
         print("색상을 입력해 주십시오. \n1: red, 2: blue, 3: green, 4: yellow, 5: black, 6: white, 7: gray, C to cancle")
         key = cv2.waitKey(0)
         if key == 67:
             pass
-        elif 49 < key < 55:
+        elif 49 <= key <= 55:
             print(f'{color}가 {colors[key-49]}로 저장되었습니다.')
 
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+    img = frame
+    rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
+    cv2.imshow('camera', img)
+    cv2.setMouseCallback('camera', train_event, img)
 
-cv2.setMouseCallback(img, train_event)
-cv2.waitKey(0)
+    key = cv2.waitKey(1) & 0xFF
+    if key == 27:
+        break
+
+cap.release()
 cv2.destroyAllWindows()
