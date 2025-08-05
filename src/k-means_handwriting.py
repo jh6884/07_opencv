3c850823b6c32de7237936f19b863723c9e03b13
shinjaehyeon <pumpkinalpha996@gmail.com>
2025-08-05 11:41:16 +0900

    손글씨 인식

--- src/k-means_handwriting.py
+++ src/k-means_handwriting.py
@@ -9,7 +9,7 @@ data, _ = mnist.getData()
 # 중지 요건 
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
 # 평균 클러스터링 적용, 10개의 그룹으로 묶음 ---②
-ret,label,center=cv2.kmeans(data,10,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
+ret,label,center=cv2.kmeans(data,10,None,criteria,100,cv2.KMEANS_RANDOM_CENTERS)
 # 중앙점 이미지 출력
 for i in range(10):
     # 각 중앙점 값으로 이미지 생성 ---③
